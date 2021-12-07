02bbf0c9ec1b5d3ee3b93cc5283ae3d1ada0d390
onur cetiner <93998298+cetineronur@users.noreply.github.com>
2021-12-07 18:50:55 +0100

    Update arrayAlt.java

--- src/arrayAlt.java
+++ src/arrayAlt.java
@@ -3,7 +3,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class arrayAlt {
+public class ArrayAlt {
 	public static void main(String[] args) {
 		/*
 		 * From a given array find all pairs whose sum is a given number verilen bir
